277d961d07b2a8b3a9b17b0dda49c41729e2d4db
Mostafa Mohamed Abdelzaher <163563684+MostafaAbd-elzaher@users.noreply.github.com>
2025-08-31 23:27:43 +0300

    Update SQLQuery1.sql

--- ETL_PROJECT_USING_SSIS/SQLQuery1.sql
+++ ETL_PROJECT_USING_SSIS/SQLQuery1.sql
@@ -6,17 +6,17 @@ CREATE TABLE DimCustomer
 (
 customer_sk int primary key identity(1,1),
 customer_id int Not null,
-first_name varchar(50) NOT NULL ,
-last_name varchar(50),
-email varchar(50) NOT NULL,
+first_name varchar(400) NOT NULL ,
+last_name varchar(400),
+email varchar(400) NOT NULL,
 address_id INT NOT NULL,
-address_status VARCHAR(200),
+address_status VARCHAR(400),
 status_id INT NOT NULL,
 street_number INT ,
-street_name VARCHAR(100),
-city VARCHAR(50), 
+street_name VARCHAR(400),
+city VARCHAR(400), 
 country_id INT ,
-country_name VARCHAR(100),
+country_name VARCHAR(400),
 valid_from DATETIME,
 valid_to DATETIME
 )
@@ -26,16 +26,16 @@ CREATE TABLE Dim_Book
 (
 book_sk int primary key identity(1,1),
 book_id int not null,
-isbn13 int ,
+isbn13 varchar(200) ,
 language_id int,
 num_pages int not null,
 publication_date date,
 publisher_id int not null,
-publisher_name varchar(200),
-language_code varchar(10),
-language_name varchar(20),
+publisher_name varchar(400),
+language_code varchar(100),
+language_name varchar(200),
 auther_id int ,
-auther_name varchar(100) not null,
+auther_name varchar(400) not null,
 valid_from datetime,
 valid_to datetime
 
@@ -44,7 +44,8 @@ valid_to datetime
 
 CREATE TABLE DateDim
 (
-    DateKey INT PRIMARY KEY,         -- Unique key in format YYYYMMDD
+	Date_sk  int primary key identity(1,1)
+    DateKey INT ,         -- Unique key in format YYYYMMDD
     FullDate DATE NOT NULL,          -- Full date value
     Day INT NOT NULL,                -- Day number (1-31)
     DayName NVARCHAR(20) NOT NULL,   -- Day name (e.g., Monday, Tuesday)
@@ -88,16 +89,7 @@ OPTION (MAXRECURSION 0);
 
 
 
-ALTER TABLE DateDim
-ADD Date_sk  int primary key identity(1,1)
 
-ALTER TABLE DateDim
-DROP CONSTRAINT  PK__DateDim__40DF45E339247F0D
--------------------------------------------------------------------
-SELECT name
-FROM sys.key_constraints
-WHERE type = 'PK' AND parent_object_id = OBJECT_ID('DateDim');
--------------------------------------------------------------------
 
 CREATE TABLE Fact_table
 (
@@ -106,6 +98,7 @@ book_sk INT NOT NULL,
 Date_sk INT NOT NULL,
 book_price DECIMAL(6,2),
 shipping_cost DECIMAL(6,2),
+order_date date,
 CONSTRAINT FK_CUSTOMER FOREIGN KEY (customer_sk) REFERENCES DimCustomer(customer_sk),
 CONSTRAINT FK_BOOK FOREIGN KEY (book_sk) REFERENCES Dim_Book(book_sk),
 CONSTRAINT FK_Date FOREIGN KEY (Date_sk) REFERENCES DateDim(Date_sk),
@@ -113,8 +106,9 @@ CONSTRAINT FK_Date FOREIGN KEY (Date_sk) REFERENCES DateDim(Date_sk),
 )
 
 
-
+-- Query for DIM_BOOK IN DESTINATION DB
 USE gravity_books
+
 SELECT B.book_id,B.title,B.isbn13,B.language_id,B.num_pages,B.publisher_id,B.publication_date,B.title,BL.language_name,BL.language_code,PU.publisher_name,B_A.author_id,AU.author_name
 FROM book B INNER JOIN book_language BL
 ON B.language_id=BL.language_id 
@@ -125,22 +119,8 @@ ON B_A.book_id=B.book_id
 INNER JOIN author AU
 ON AU.author_id=B_A.author_id
 
-use Gravity_warehouse
 
-sp_help dim_book
-
-SP_HELP BOOK
-
-use Gravity_warehouse
-ALTER TABLE DIM_BOOK
-ALTER COLUMN isbn13 varchar(20)
-
-use Gravity_warehouse
-ALTER TABLE DIM_BOOK
-ALTER COLUMN publisher_name varchar(400) not null
-
-
-
+-- Query for DIM_CUSTOMER IN DESTINATION DB
 USE gravity_books
 SELECT C.customer_id,C.email,C.first_name,C.last_name,CA.address_id,
 CA.status_id,ad.address_status,[add].street_name,[add].street_number,
@@ -157,12 +137,7 @@ on co.country_id=[add].country_id
 
 
 
-use Gravity_warehouse
-ALTER TABLE DimCustomer
---ALTER COLUMN first_name varchar(200)
-ALTER COLUMN last_name varchar(200)
-
-
+-- Query for FACT_TABLE IN DESTINATION DB
 USE gravity_books
 
 select customer_id , price , OL.book_id  , SM.cost ,order_date 
@@ -172,15 +147,11 @@ on CO.order_id = OL.order_id
 inner join book B
 on OL.book_id = B.book_id
 inner join shipping_method SM
-on CO.shipping_method_id = SM.method_id
-
-use Gravity_warehouse
-alter table fact_table
-add order_date date
+on CO.shipping_method_idÂ =Â SM.method_id
 
 
 
-
+--FINAL TABLES AFTER THE ETL_PROCESS
 use Gravity_warehouse
 
 SELECT * FROM Dim_Book
